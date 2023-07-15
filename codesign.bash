3c3b3d6fea67eaf5788c43d11a2209b99c4ad61f
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2023-07-15 16:30:48 +0900

    a

--- codesign.bash
+++ codesign.bash
@@ -19,18 +19,18 @@ target_file_glob="$1"
 
 # eSignerCKAのセットアップ
 INSTALL_DIR='C:\Users\runneradmin\eSignerCKA'
-curl -LO "https://github.com/SSLcom/eSignerCKA/releases/download/v1.0.6/SSL.COM-eSigner-CKA_1.0.6.zip"
-unzip -o SSL.COM-eSigner-CKA_1.0.6.zip
-rm SSL.COM-eSigner-CKA_1.0.6.zip
-mv *eSigner*CKA_*.exe eSigner_CKA_Installer.exe
-powershell "& ./eSigner_CKA_Installer.exe /CURRENTUSER /VERYSILENT /SUPPRESSMSGBOXES /DIR="$INSTALL_DIR" | Out-Null"
-rm eSigner_CKA_Installer.exe
-
-powershell "
-    & '$INSTALL_DIR\eSignerCKATool.exe' config -mode product -user '$ESIGNERCKA_USERNAME' -pass '$ESIGNERCKA_PASSWORD' -totp '$ESIGNERCKA_TOTP_SECRET' -key '$INSTALL_DIR\master.key' -r
-    & '$INSTALL_DIR\eSignerCKATool.exe' unload
-    & '$INSTALL_DIR\eSignerCKATool.exe' load
-"
+if [ ! -d "$INSTALL_DIR" ]; then
+    curl -LO "https://github.com/SSLcom/eSignerCKA/releases/download/v1.0.6/SSL.COM-eSigner-CKA_1.0.6.zip"
+    unzip -o SSL.COM-eSigner-CKA_1.0.6.zip
+    mv *eSigner*CKA_*.exe eSigner_CKA_Installer.exe
+    powershell "
+        & ./eSigner_CKA_Installer.exe /CURRENTUSER /VERYSILENT /SUPPRESSMSGBOXES /DIR="$INSTALL_DIR" | Out-Null
+        & '$INSTALL_DIR\eSignerCKATool.exe' config -mode product -user '$ESIGNERCKA_USERNAME' -pass '$ESIGNERCKA_PASSWORD' -totp '$ESIGNERCKA_TOTP_SECRET' -key '$INSTALL_DIR\master.key' -r
+        & '$INSTALL_DIR\eSignerCKATool.exe' unload
+        & '$INSTALL_DIR\eSignerCKATool.exe' load
+    "
+    rm SSL.COM-eSigner-CKA_1.0.6.zip eSigner_CKA_Installer.exe
+fi
 
 # # 指定ファイルに署名する
 # function codesign() {
