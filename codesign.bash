9a04923211175ced5f475106636662c7abf6cb0f
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2023-07-15 15:03:45 +0900

    a

--- codesign.bash
+++ codesign.bash
@@ -2,11 +2,11 @@
 
 set -eu
 
-if [ -v "${CERT_BASE64}" ]; then
+if [ ! -v CERT_BASE64 ]; then
     echo "CERT_BASE64が未定義です"
     exit 1
 fi
-if [ -v "${CERT_PASSWORD}" ]; then
+if [ ! -v CERT_PASSWORD ]; then
     echo "CERT_PASSWORDが未定義です"
     exit 1
 fi
@@ -17,33 +17,37 @@ if [ $# -ne 1 ]; then
 fi
 target_file_glob="$1"
 
-# 証明書
-CERT_PATH=cert.pfx
-echo -n "$CERT_BASE64" | base64 -d - > $CERT_PATH
+# eSignerCKAのインストール
+curl -LO "https://github.com/SSLcom/eSignerCKA/releases/download/v1.0.6/SSL.COM-eSigner-CKA_1.0.6.zip"
+unzip -o SSL.COM-eSigner-CKA_1.0.6.zip
+rm SSL.COM-eSigner-CKA_1.0.6.zip
+mv eSigner_CKA_*/*.exe eSigner_CKA_Installer.exe
+./eSigner_CKA_Installer.exe /CURRENTUSER /VERYSILENT /SUPPRESSMSGBOXES >/dev/null 2>&1
+rm -r eSigner_CKA_* eSigner_CKA_Installer.exe
 
-# 指定ファイルに署名する
-function codesign() {
-    TARGET="$1"
-    SIGNTOOL=$(find "C:/Program Files (x86)/Windows Kits/10/App Certification Kit" -name "signtool.exe" | sort -V | tail -n 1)
-    powershell "& '$SIGNTOOL' sign /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $CERT_PATH /p $CERT_PASSWORD '$TARGET'"
-}
+# # 指定ファイルに署名する
+# function codesign() {
+#     TARGET="$1"
+#     SIGNTOOL=$(find "C:/Program Files (x86)/Windows Kits/10/App Certification Kit" -name "signtool.exe" | sort -V | tail -n 1)
+#     powershell "& '$SIGNTOOL' sign /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $CERT_PATH /p $CERT_PASSWORD '$TARGET'"
+# }
 
-# 指定ファイルが署名されているか
-function is_signed() {
-    TARGET="$1"
-    SIGNTOOL=$(find "C:/Program Files (x86)/Windows Kits/10/App Certification Kit" -name "signtool.exe" | sort -V | tail -n 1)
-    powershell "& '$SIGNTOOL' verify /pa '$TARGET'" || return 1
-}
+# # 指定ファイルが署名されているか
+# function is_signed() {
+#     TARGET="$1"
+#     SIGNTOOL=$(find "C:/Program Files (x86)/Windows Kits/10/App Certification Kit" -name "signtool.exe" | sort -V | tail -n 1)
+#     powershell "& '$SIGNTOOL' verify /pa '$TARGET'" || return 1
+# }
 
-# 署名されていなければ署名
-ls $target_file_glob | while read target_file; do
-    if is_signed "$target_file"; then
-        echo "署名済み: $target_file"
-    else
-        echo "署名: $target_file"
-        codesign "$target_file"
-    fi
-done
+# # 署名されていなければ署名
+# ls $target_file_glob | while read target_file; do
+#     if is_signed "$target_file"; then
+#         echo "署名済み: $target_file"
+#     else
+#         echo "署名: $target_file"
+#         codesign "$target_file"
+#     fi
+# done
 
-# 証明書を消去
-rm $CERT_PATH
+# # 証明書を消去
+# rm $CERT_PATH
