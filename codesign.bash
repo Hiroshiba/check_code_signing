ae5fc9f8f9b6a9026081c44b9dcf9dca028b0866
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2023-07-15 19:45:40 +0900

    a

--- codesign.bash
+++ codesign.bash
@@ -48,7 +48,8 @@ fi
 # 指定ファイルに署名する
 function codesign() {
     TARGET="$1"
-    SIGNTOOL=$(find "C:/Program Files (x86)/Windows Kits/10/App Certification Kit" -name "signtool.exe" | sort -V | tail -n 1)
+    # SIGNTOOL=$(find "C:/Program Files (x86)/Windows Kits/10/App Certification Kit" -name "signtool.exe" | sort -V | tail -n 1)
+    SIGNTOOL='C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe'
     powershell "
         & '$INSTALL_DIR\eSignerCKATool.exe' config -mode product -user '$ESIGNERCKA_USERNAME' -pass '$ESIGNERCKA_PASSWORD' -totp '$ESIGNERCKA_TOTP_SECRET' -key '$INSTALL_DIR\master.key' -r
         & '$INSTALL_DIR\eSignerCKATool.exe' unload
