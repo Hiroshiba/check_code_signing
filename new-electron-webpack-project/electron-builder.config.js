319edf6622eb5604fb57dd02b63bea49bd12acee
Hiroshiba <hihokaruta@gmail.com>
2023-10-03 02:23:11 +0900

    a

--- new-electron-webpack-project/electron-builder.config.js
+++ new-electron-webpack-project/electron-builder.config.js
@@ -1,9 +1,11 @@
 const certificateSha1 = null; // REPLACE HERE
+const signingHashAlgorithms = null; // REPLACE HERE
 
 /** @type {import("electron-builder").Configuration} */
 const builderOptions = {
   win: {
     certificateSha1,
+    signingHashAlgorithms,
   },
 };
 
